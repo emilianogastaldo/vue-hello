0b7c6af732f14b537e97a8cf3880ba4cdb0fac31
emilianogastaldo <emiliano0303@gmail.com>
2024-01-19 15:21:06 +0100

    aggiungo nome  e data all'app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // Importo Vue e creo l'infrastruttura per utilizzarlo
 const { createApp } = Vue;
-const app = createApp({});
+const app = createApp({
+    name: 'Hello Vue',
+    data() {
+        return {
+            message: 'Hello VUE',
+            image: 'https://coursereport-production.imgix.net/uploads/school/logo/681/original/Pittogramma-RGB-1080-BLUE-circle.png?w=72&h=72'
+        }
+    }
+});
 app.mount('#root');
